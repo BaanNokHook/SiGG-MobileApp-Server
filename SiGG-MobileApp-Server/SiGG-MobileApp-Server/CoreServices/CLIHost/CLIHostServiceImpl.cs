3aa7be7d5904b9ea342d70573b2bf0531008608c
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-01-20 22:37:13 +0700

    SiGG-MobileApp-Server-Service-module-CoreService-CLIHost-CLIHostServiceImpl

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/CoreServices/CLIHost/CLIHostServiceImpl.cs
@@ -0,0 +1,56 @@
+﻿using SiGG_MobileApp_Server.CoreServices.CLIHost;
+using SocketAppServer.CoreServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SiGG_MobileApp_Server.CoreServices.CLIHost.CLICommand;
+
+namespace SiGG_MobileApp_Server.CoreServices.CLIHost
+{
+    internal class CLIHostServiceImpl : ICLIHostService
+    {
+        private List<CLICCommand> commands;
+
+        public CLIHostServiceImpl()
+        {
+            commands = new List<CLICommand>();
+        }
+
+        public CLICommand GetCommand(string commandText)
+        {
+            return commands.FirstOrDefault(c => c.CommandText.Equals(commandText));
+        }
+
+        public void RegisterCLICommand(string commandText, string commandDescription, ICLIClient cliClient)
+        {
+            if (string.IsNullOrEmpty(commandText))
+                throw new Exception("CommandText is null");
+            if (cliClient == null)
+                throw new Exception("Client is null");
+            if (GetCommand(commandText) != null)
+                throw new Exception("This command is already in use by another CLI client");
+            if (RegisteredCommands().Any(c => c.ExecutorClient.GetType() == cliClient.GetType()))
+                throw new Exception($"CLI client type '{cliClient.GetType().FullName}' is already in use");
+
+            commands.Add(new CLICommand(commandText, commandDescription, cliClient));
+        }
+
+        public IReadOnlyCollection<CLICommand> RegisteredCommands()
+        {
+            return commands.AsReadOnly();
+        }
+
+        private bool isCliBusy = false;
+        public void SetCLIBusy(bool isBusy)
+        {
+            isCliBusy = isBusy;
+        }
+
+        public bool IsCLIBusy()
+        {
+            return isCliBusy;
+        }
+    }
+}
