f4a864ec1002b1f86a910edf16337674d342b108
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-01-20 22:02:09 +0700

    SiGG-MobileApp-Server-Service-module-CoreService-CLIHost-CLICommand

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/CoreServices/CLIHost/CLICommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.CoreServices.CLIHost
+{
+    public class CLICommand
+    {
+        public class ICILClient
+        {
+            public string CommandText { get; private set; }  
+            public string CommandDescription { get; private set; } 
+
+            public ICILClient ExecutorClient { get; private set; }   
+
+            public CLICommand(string commandText, string commandDescription, ICILClient exeutorClient)
+            {
+                CommandText = commandText;
+                CommandDescription = commandDescription;
+                ExecutorClient = exeutorClient;  
+            }
+        }
+    }
+}
