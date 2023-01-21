6beed4937a80ff790673cc97d32435f92f23bea7
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-01-21 16:03:01 +0700

    SiGG-MobileApp-Server-Service-module-CoreService-CoreServer-LockedActionException

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/CoreServices/CoreServer/LockedActionException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.CoreServices.CoreServer
+{
+    public class LockedActionException : Exception
+    {
+        public LockedActionException(string message) : base(message)
+        {
+        }
+    }
+}
