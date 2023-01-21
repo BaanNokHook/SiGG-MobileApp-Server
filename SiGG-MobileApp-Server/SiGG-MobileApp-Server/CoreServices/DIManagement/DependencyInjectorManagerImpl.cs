4e1d16ff52a68ac0c108c9c7dcd61bafc0a6bf3b
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-01-21 20:43:54 +0700

    SiGG-MobileApp-Server-Service-module-CoreService-DIManagement-DependencyInjectorManagerImpl

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/CoreServices/DIManagement/DependencyInjectorManagerImpl.cs
@@ -0,0 +1,34 @@
+﻿using SocketAppServer.CoreServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.CoreServices.DIManagement
+{
+    internal class DependencyInjectorManagerImpl : IDependencyInjectionService  
+    {
+        private List<IDependencyInjectorMaker> dependencyInjectors = null;   
+
+        public DependencyInjectorManagerImpl()
+        {
+            dependencyInjectors = new List<IDependencyInjectorMaker>(10);   
+        }
+
+        public void AddDependencyInjector(IDependencyInjectorMaker injectorMaker)
+        {
+            if (string.IsNullOrEmpty(injectorMaker.ControllerNsame))
+                throw new Exception("It is no longer possible to use to use empty strings or nulls for ControllerName, If your intention is to inicate ALL, use the '*' character inside the string");
+            dependencyInjectors.Add(injectorMaker);   
+        }  
+
+        public IDependencyInjectorMaker GetInjectorMaker(string controllerName)
+        {
+            IDependencyInjectorMaker controllerMaker = dependencyInjectors.FirstOrDefault(di => di.ControllerName.Equals(controllerName));
+            if (controllerMaker == null)
+                controllerMaker = dependencyInjectors.FirstOrDefault(di => di.ControllerName.Equals("*"));
+            return controllerMaker;
+        }
+    }
+}
