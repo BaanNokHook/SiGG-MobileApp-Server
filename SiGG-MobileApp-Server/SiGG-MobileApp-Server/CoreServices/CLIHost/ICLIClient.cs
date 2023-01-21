cc88f515b155d69ad6beea307e241150d8ebd0cd
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-01-21 09:53:15 +0700

    SiGG-MobileApp-Server-Service-module-CoreService-CLIHost-ICLIClient

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/CoreServices/CLIHost/ICLIClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.CoreServices.CLIHost
+{
+    internal class ICLIClient
+    {
+        void Activate();
+    }
+}
+
+
+
+
+
+
+
+
