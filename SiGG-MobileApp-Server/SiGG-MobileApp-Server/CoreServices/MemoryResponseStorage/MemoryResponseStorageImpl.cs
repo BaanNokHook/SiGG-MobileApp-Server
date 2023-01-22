1dec6f8759a603dc773f50cf2c25b7a6fc87d9ce
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-01-22 10:13:42 +0700

    SiGG-MobileApp-Server-Service-module-CoreService-InterceptorManagement-MemoryResponseStorageImpl

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/CoreServices/MemoryResponseStorage/MemoryResponseStorageImpl.cs
@@ -0,0 +1,60 @@
+﻿using SocketAppServer.CoreServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.CoreServices.MemoryResponseStorage
+{
+    internal class MemoryResponseStorageImpl : IMemoryResponseStorage
+    {
+        private object lck = new object();
+
+        private List<MemoryResponseStorageItem> Items { get; set; }   
+
+        public MemeoryResponseStorageImpl()
+        {
+            Items = new List<MemoryResponseStorageItem>();
+            IServiceManager services = ServiceManager.GetInstance();
+            IControllerManager manager = services.GetService<IControllerManager>();
+            manager.RegisterControllerter(typeof(ResponseStorageController));  
+        }
+
+        public void CreateStorage(string id, string content)
+        {
+            lock (lck)
+            {
+                if (Items.Any(i => i.Id.Equals(id)))
+                    throw new Exception("There is already a pending recovery response for the created ID.");
+                Items.Add(new MemoryResponseStorageItem(id, content));   
+            }
+        }
+
+        public string Read(string id, int length)
+        {
+            lock (lck)
+            {
+                MemoryResponseStorageItem item = Items.FirstOrDefault(
+                        i => i.Id.Equals(id));
+                if (item == null)
+                    return null;
+
+                string returnContent = null;
+                if (length >= item.ResponseContent.Length)
+                {
+                    returnContent = item.ResponseContent;
+                    Items.Remove(item);
+                    return returnContent;
+                }
+
+                returnContent = item.ResponseContent.Substring(0, length);
+                item.SetContent(item.ResponseContent.Substring(length,
+                    item.ResponseContent.Length - length));
+
+                return returnContent;
+            }
+        }
+
+    }
+}
