35626bdb1cf81f2373cb0702cd169efce5eb2d60
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-01-22 12:50:50 +0700

    SiGG-MobileApp-Server-Service-module-CoreService-TelemetryManagement-TelemetryManagementImpl

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/CoreServices/TelemetryManagement/TelemetryManagementImpl.cs
@@ -0,0 +1,37 @@
+﻿using SocketAppServer.CoreServices;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.CoreServices.TelemetryManagement
+{
+    public class TelemetryManagementImpl : ITelemetryManagement   
+    {
+        private Type provider;
+        private IServiceManager manager;  
+
+        public TelemetryManagementImpl()
+        {
+            SetProviderType(typeof(DefaultTelemetryServicesProvider));  
+        }
+
+        public void Initialize()
+        {
+            manager = ServiceManager.GetInstance();
+            manager.Bind<ITelemetryServicesProvider>(provider, true);
+            manager.Bind<ITelemetryDataCollector>(typeof(TelemetryDataCollectorImpl), true);
+
+            IScheduledTaskManager taskManager = manager.GetService<IScheduledTaskManager>();
+            taskManager.AddScheduledTask(new HWUsageCollectorTask());
+            taskManager.AddScheduledTask(new TelemetryProcessorTask());   
+        }
+
+        public void SetProviderType(Type providerType)
+        {
+            provider = providerType;
+        }
+    }
+}
