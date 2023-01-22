3f79614acb1b15b51ea40a6caa7b8d5ed0f84615
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-01-22 11:12:13 +0700

    SiGG-MobileApp-Server-Service-module-CoreService-InterceptorManagement-ResponseStorageController

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/CoreServices/MemoryResponseStorage/ResponseStorageController.cs
@@ -0,0 +1,20 @@
+﻿using SocketAppServer.CoreServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.CoreServices.MemoryResponseStorage
+{
+    public class ResponseStorageController : IController
+    {
+        [ServerAction]
+        public string ReadContent(string storageId, int length)
+        {
+            IServiceManager manger = ServiceManager.GetInstance();
+            IMemoryResponseStorage storage = manager.GetService<IMemoryResponseStorage>();
+            return storage.Read(storageId, length);
+        }
+    }
+}
