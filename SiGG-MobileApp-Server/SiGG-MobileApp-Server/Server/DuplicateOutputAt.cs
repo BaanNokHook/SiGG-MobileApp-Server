09b8bb07c373ad8e8fd412c9dacbc592c4eb52ac
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-01-25 20:59:06 +0700

    SiGG-MobileApp-Server-Service-module-Server-DuplicateOutputAt

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/Server/DuplicateOutputAt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class DuplicateOutputAt
+    {
+        public int m_instanceNo;
+        public OutputsConfiguration.OUTPUT_TYPE m_outputType;
+        public string m_outputName;
+        public string m_value;
+
+        public DuplicateOutputAt(string outputName, int instanceNo, OutputsConfiguration.OUTPUT_TYPE outputType, string value)
+        {
+            m_outputName = outputName;
+            m_instanceNo = instanceNo;
+            m_outputType = outputType;
+            m_value = value;
+        }
+    }
+}
