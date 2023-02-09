535a82514e69216410e05b21921d21d6dfa26257
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-09 19:16:14 +0700

    SiGG-MobileApp-Server-Service-module-Server-RecordMapSettingData

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/Server/RecordMapSettingData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.Server
+{
+    public class RecordMapSettingData
+    {
+        public string m_nodeName;
+        public string m_prefix;
+        public string m_input;
+        public string m_flags;
+        public string m_section;
+
+        public RecordMapSettingData(string nodeName, string prefix, string input, string flags, string section)
+        {
+            m_nodeName = nodeName;
+            m_prefix = prefix;
+            m_input = input;
+            m_flags = flags;
+            m_section = section;
+        }
+    }
+}
