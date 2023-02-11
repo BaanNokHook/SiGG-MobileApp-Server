18bba58fc9a3207917c28e7b03ac96ca4c2851eb
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-12 06:57:18 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-module-Hosting-ISocketServerHostBuilder

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/Hosting/ISocketServerHostBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.Hosting
+{
+    public interface ISocketServerHostBuilder
+    {
+        ISocketServerHostBuilder UseStartup<TStartup>() where TStartup : class;
+        void Run();
+    }
+}
