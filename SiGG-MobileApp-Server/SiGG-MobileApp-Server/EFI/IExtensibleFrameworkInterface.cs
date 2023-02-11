6ecf859e3b02cd0e503e53c1b7ec12db1635cf30
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-12 06:30:05 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-module-EFI-IExtensibleFrameworkInterface

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/EFI/IExtensibleFrameworkInterface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.EFI
+{
+    public interface IExtensibleFrameworkInterface
+    {
+        string ExtensionName { get; }
+
+        string ExtensionVersion { get; }
+
+        string ExtensionPublisher { get; }
+
+        string MinServerVersion { get; }
+
+        void Load(IServiceManager manager);
+    }
+}
