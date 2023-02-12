ac4f65247d3412f809489cc8f2d7f850823bb31e
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-12 12:03:20 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-module-LoadBalancingServices-INotifiableSubServerRequirement

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/LoadBalancingServices/INotifiableSubServerRequirement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.LoadBalancingServices
+{
+    public interface INotifiableSubServerRequirement
+    {
+        SubServer StartNewInstance();
+
+        void StopInstance(SubServer server);
+    }
+}
