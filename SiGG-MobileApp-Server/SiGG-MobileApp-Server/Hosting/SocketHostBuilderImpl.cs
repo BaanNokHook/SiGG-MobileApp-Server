824ef1037e0bccad5e6373ce299fc432bdfdc2c2
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-12 08:51:54 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-module-Hosting-SocketHostBuilderImpl

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/Hosting/SocketHostBuilderImpl.cs
@@ -0,0 +1,33 @@
+﻿using SiGG_MobileApp_Server.CoreServices.CoreServer;
+using SocketAppServer.CoreServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.Hosting
+{
+    internal class SocketHostBuilderImpl : ISocketServerHostBuilder
+    {
+        private AppServerConfigurator configurator;
+        public void Run()
+        {
+            IServiceManager manager = configurator.Services;
+            configurator.ConfigureServices(manager);
+
+            ServerConfiguration config = configurator.GetServerConfiguration();
+
+            ICoreServerService server = manager.GetService<ICoreServerService>("realserver");
+            server.SetConfiguration(config);
+            server.Start();
+        }
+
+        public ISocketServerHostBuilder UseStartup<TStartup>() where TStartup : class
+        {
+            Type t = typeof(TStartup);
+            configurator = (AppServerConfigurator)Activator.CreateInstance(t);
+            return this;
+        }
+    }
+}
