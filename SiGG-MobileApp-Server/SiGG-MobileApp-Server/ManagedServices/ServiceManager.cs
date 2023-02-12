8056cd269b805f726c1ed19f3d919b60569ee98f
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-12 16:00:06 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ManagedServices-ServiceManager

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ManagedServices/ServiceManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ManagedServices
+{
+    public sealed class ServiceManager
+    {
+        private static IServiceManager _instance = null;
+        public static IServiceManager GetInstance()
+        {
+            if (_instance == null)
+                _instance = new ServiceManagerImpl();
+            return _instance;
+        }
+    }
+}
