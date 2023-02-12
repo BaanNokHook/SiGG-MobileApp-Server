22d366448c0067e4b8e41fe76abe3e8c61d1b4b7
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-12 21:45:46 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ScheduledServices-ScheduledTaskInterval

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ScheduledServices/ScheduledTaskInterval.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ScheduledServices
+{
+    public class ScheduledTaskInterval
+    {
+        public double Interval { get; private set; }
+        public int Days { get; private set; }
+        public int Hours { get; private set; }
+        public int Minutes { get; private set; }
+        public int Seconds { get; private set; }
+
+        public ScheduledTaskInterval(int days, int hours, int minutes, int seconds)
+        {
+            Interval = new TimeSpan(days, hours, minutes, seconds, 0).TotalMilliseconds;
+            Days = days;
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        public override string ToString()
+        {
+            return $"Interval: {Days} days, {Hours} hours, {Minutes} minutes and {Seconds} seconds";
+        }
+    }
+}
