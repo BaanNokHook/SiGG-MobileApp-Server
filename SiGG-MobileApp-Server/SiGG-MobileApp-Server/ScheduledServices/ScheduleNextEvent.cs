33867754f757c9b7806a11ac12b68f9c8ef7ec4f
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-12 22:00:03 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ScheduledServices-ScheduleNextEvent

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ScheduledServices/ScheduleNextEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ScheduledServices
+{
+    internal class ScheduleNextEvent
+    {
+        public string TaskName { get; set; }
+
+        public DateTime NextEvent { get; set; }
+
+        public ScheduleNextEvent(string taskName, DateTime nextEvent)
+        {
+            TaskName = taskName;
+            NextEvent = nextEvent;
+        }
+    }
+}
