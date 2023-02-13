7febe54055122f759f82620d97d8762f530106a4
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-14 06:06:57 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-Security-IUserServerRepository

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/Security/IUserServerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.Security
+{
+    public interface IServerUserRepository
+    {
+        ServerUser Authenticate(string userNameOrEmail, string password);
+
+        void OnSuccessFulAuthentication(string token);
+    }
+}
