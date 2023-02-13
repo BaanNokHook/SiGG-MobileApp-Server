60c25ed12dac8665b7f79305cdf3f07fdd37d80b
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-14 06:45:37 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-Security-SecurityTokenInterceptor

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/Security/SecurityTokenInterceptor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.Security
+{
+    internal class SecurityTokenInterceptor : IHandlerInterceptor
+    {
+        public string ControllerName { get { return "*"; } }
+        public string ActionName { get { return "*"; } }
+
+        public InterceptorHandleResult PreHandle(SocketRequest socketRequest)
+        {
+            string controllerName = socketRequest.Controller.GetType().Name;
+            string action = socketRequest.Action;
+            if (controllerName.Equals("AuthorizationController") ||
+                controllerName.Equals("ServerInfoController"))
+                return new InterceptorHandleResult(false, true, "", "");
+
+            var paramToken = socketRequest.Parameters.FirstOrDefault(p => p.Name.Equals("authorization"));
+            if (paramToken == null)
+                return new InterceptorHandleResult(true, false, "Unauthorized. Check 'authorization' parameter in request body", "");
+
+            string token = paramToken.Value.ToString();
+            if (!TokenRepository.Instance.IsValid(token, ref socketRequest))
+                return new InterceptorHandleResult(true, false, "Invalid or expired token. Check 'authorization' parameter in request body", "");
+
+            return new InterceptorHandleResult(false, true, "Authorized", "");
+        }
+    }
+}
