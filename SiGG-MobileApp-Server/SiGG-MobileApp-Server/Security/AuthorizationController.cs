1183270943191442cabadd2549a6aafbef9636e2
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-13 18:31:32 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-Security-AuthorizationController

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/Security/AuthorizationController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.Security
+{
+    public class AuthorizationController : IController
+    {
+        public ActionResult IsValidToken(string token, SocketRequest request)
+        {
+            bool valid = TokenRepository.Instance.IsValid(token, ref request);
+            string msg = (valid
+                ? "You have a valid token"
+                : "You have a invalid token");
+            return ActionResult.Json(new OperationResult(valid, (valid ? 600 : 500), msg), (valid ? 600 : 500),
+                msg);
+        }
+
+        public ActionResult Authorize(string user, string password,
+            SocketRequest request)
+        {
+            IServiceManager manager = ServiceManager.GetInstance();
+            ISecurityManagementService service = manager.GetService<ISecurityManagementService>();
+
+            var serverUser = service.Authenticate(user, password);
+            if (serverUser == null)
+                return ActionResult.Json(new OperationResult(string.Empty, 500, "Invalid user"), 500, "Invalid user");
+            var token = TokenRepository.Instance.AddToken(serverUser, ref request);
+
+            try
+            {
+                service.OnSuccessFulAuthentication(token.UserToken);
+            }
+            catch { }
+
+            return ActionResult.Json(new OperationResult(token.UserToken, 600, "Authorization success. Use this token to authenticate in next requests."));
+        }
+
+        [NotListed]
+        [ServerAction]
+        public void ReplicateToken(string token)
+        {
+            TokenRepository.Instance.AddReplicatedToken(token);
+        }
+    }
+}
