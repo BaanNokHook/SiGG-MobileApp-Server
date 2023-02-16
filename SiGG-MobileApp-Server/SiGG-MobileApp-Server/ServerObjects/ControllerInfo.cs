75104f63a22d312878c34b13417c4b7b1ff55c8d
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-16 21:09:15 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-ControllerInfo

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/ControllerInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    internal class ControllerInfo
+    {
+        public string ControllerName { get; set; }
+        public string ControllerClass { get; set; }
+        public List<string> ControllerActions { get; set; }
+
+        public ControllerInfo()
+        {
+            ControllerActions = new List<string>();
+        }
+    }
+}
