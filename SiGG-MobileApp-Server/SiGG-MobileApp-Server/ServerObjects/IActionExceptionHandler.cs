71e170b3cb7d0cabcd2d55b1337e1e305eb22f95
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-16 21:40:50 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-IActionExceptionHandler

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/IActionExceptionHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    public interface IActionExceptionHandler
+    {
+        OperationResult Handle(Exception exception, SocketRequest request);
+    }
+}
