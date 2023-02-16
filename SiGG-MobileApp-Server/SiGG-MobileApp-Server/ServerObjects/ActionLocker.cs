0d2a7f1aec8cee832fa9c90a90c695aa5953d334
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-16 17:56:42 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-ActionLocker

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/ActionLocker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    public class ActionLocker
+    {
+        private static List<KeyValuePair<IController, string>> lockedActions = new List<KeyValuePair<IController, string>>();
+
+        internal static bool ActionHasLock(IController controller, string actionName)
+        {
+            var lckd = lockedActions.FirstOrDefault(l => l.Key.GetType().Name.Equals(controller.GetType().Name) &&
+              l.Value.Equals(actionName));
+
+            return (lckd.Key != null);
+        }
+
+        public static void AddLock(IController controller, string actionName)
+        {
+            IServiceManager manager = ServiceManager.GetInstance();
+            ICoreServerService coreServer = manager.GetService<ICoreServerService>("realserver");
+
+            if (coreServer.GetConfiguration().IsSingleThreaded)
+                throw new Exception("Action blocking not allowed for single-threaded servers");
+
+            lockedActions.Add(new KeyValuePair<IController, string>(controller, actionName));
+        }
+
+        public static void ReleaseLock(IController controller, string actionName)
+        {
+            try
+            {
+                var lckd = lockedActions.FirstOrDefault(l => l.Key.GetType().Name.Equals(controller.GetType().Name) &&
+                   l.Value.Equals(actionName));
+
+                if (lckd.Key != null)
+                    lockedActions.Remove(lckd);
+            }
+            catch { }
+        }
+    }
+}
