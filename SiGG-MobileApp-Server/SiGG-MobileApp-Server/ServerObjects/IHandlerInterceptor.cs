869f33b69f19034988dc0668599f367aea02cd1a
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-17 16:47:51 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-IHandlerInterceptor

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/IHandlerInterceptor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    public interface IHandlerInterceptor
+    {
+        string ControllerName { get; }
+
+        string ActionName { get; }
+
+        InterceptorHandleResult PreHandle(SocketRequest socketRequest);
+    }
+}
