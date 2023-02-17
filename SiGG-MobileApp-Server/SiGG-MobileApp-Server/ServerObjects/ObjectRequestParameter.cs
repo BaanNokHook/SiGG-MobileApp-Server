8be5e4df7637f6f96255b4209ca139ca26db0407
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-17 17:09:36 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-ObjectRequestParameter

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/ObjectRequestParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    internal class ObjectRequestParameter
+    {
+        public string Alias { get; private set; }
+        public string TypeName { get; private set; }
+
+        public ObjectRequestParameter(string alias, string name)
+        {
+            Alias = alias;
+            TypeName = name;
+        }
+    }
+}
