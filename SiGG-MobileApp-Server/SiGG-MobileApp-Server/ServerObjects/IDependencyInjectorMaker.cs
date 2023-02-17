40806680bec584c6611ffc4992a143b4324762ec
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-17 16:34:57 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-IDependencyInjectorMaker

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/IDependencyInjectorMaker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    public interface IDependencyInjectorMaker
+    {
+        string ControllerName { get; }
+
+        object[] BuildInjectValues(RequestBody body);
+    }
+}
