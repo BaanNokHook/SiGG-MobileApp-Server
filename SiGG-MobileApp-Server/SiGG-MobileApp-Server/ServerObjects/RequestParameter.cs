6ced151a1cd8d303c15c5b6725c6421e5c1cbef2
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-17 17:36:32 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-RequestParameter

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/RequestParameter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    public class RequestParameter
+    {
+        public string Name { get; set; }
+        public object Value { get; set; }
+
+        internal RequestParameter(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        internal bool IsComplexType()
+        {
+            return Name.Contains(".");
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        internal string GetAliasName()
+        {
+            int pIndex = Name.IndexOf('.');
+            int ptIndex = pIndex + 1;
+            string aliasName = Name.Substring(0, pIndex);
+            return aliasName;
+        }
+
+        internal string GetParameterProperyName()
+        {
+            int pIndex = Name.IndexOf('.');
+            int ptIndex = pIndex + 1;
+            string propertyName = Name.Substring(ptIndex, Name.Length - ptIndex);
+            return propertyName;
+        }
+
+        public RequestParameter()
+        {
+
+        }
+    }
+}
