e18a15bf79089acf6e066c5359071758a6576c46
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-17 21:01:42 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-ServerAction

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/ServerAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    /// <summary>
+    /// Allows you to define void or object as an action return, instead of ActionResult
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ServerAction : Attribute
+    {
+
+        public ServerAction()
+        {
+            if (AppServerConfigurator.DefaultExceptionHandlerType != null)
+                ExceptionHandler = AppServerConfigurator.DefaultExceptionHandlerType;
+        }
+
+
+        /// <summary>
+        /// Customized error code for response to the client, 
+        /// instead of using the default code of the server (500)
+        /// </summary>
+        public int DefaultErrorCode { get; set; }
+
+        /// <summary>
+        /// Defines the type of an IActionExceptionHandler for handling 
+        /// exceptions thrown by this action
+        /// </summary>
+        public Type ExceptionHandler { get; set; }
+    }
+}
