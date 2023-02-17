4ecbef89f521d72a0a453fc0e234b9d207c7da50
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-17 17:37:20 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-ResponseStatus

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/ResponseStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    internal class ResponseStatus
+    {
+        public const int SUCCESS = 600;
+        public const int ERROR = 500;
+        public const int LOCKED = 550;
+    }
+}
