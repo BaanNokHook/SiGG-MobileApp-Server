c089a48cff50a4de9482cb6bd1f64db5d7e9fc34
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-17 17:26:02 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-RequestBody

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/RequestBody.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    public class RequestBody
+    {
+        [JsonProperty]
+        public string Action { get; internal set; }
+
+        [JsonProperty]
+        public string Controller { get; internal set; }
+
+        [JsonProperty]
+        public List<RequestParameter> Parameters { get; internal set; }
+
+        [JsonProperty]
+        public string InTo { get; internal set; }
+
+        public RequestBody()
+        {
+
+        }
+    }
+}
