26c0592da50bc19ee723b8808ea2648d84b1bd63
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-17 17:21:18 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-OperationResult

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/OperationResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    public struct OperationResult
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public object Entity { get; set; }
+
+        public OperationResult(object entity, int status, string message)
+        {
+            Status = status;
+            Entity = entity;
+            Message = message;
+        }
+    }
+}
