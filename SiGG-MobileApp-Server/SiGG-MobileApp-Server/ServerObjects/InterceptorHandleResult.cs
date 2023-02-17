aca9a01aca59d6c6fa66873bb2d019b5a90f396a
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-17 16:55:21 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-InterceptorHandleResult

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/InterceptorHandleResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    public class InterceptorHandleResult
+    {
+        internal bool CancelActionInvoke { get; set; }
+        public bool ResponseSuccess { get; }
+        internal string Message { get; set; }
+
+        internal object Data { get; set; }
+
+        public InterceptorHandleResult(bool cancelActionInvoke,
+            bool responseSuccess,
+            string message, object data)
+        {
+            CancelActionInvoke = cancelActionInvoke;
+            ResponseSuccess = responseSuccess;
+            Message = message;
+            Data = data;
+        }
+    }
+}
