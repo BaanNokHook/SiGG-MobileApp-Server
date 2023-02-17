a4455714f114ba391825cdac1156b989fcce7a29
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-17 17:01:24 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-JsonResult

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/JsonResult.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    public class JsonResult : ActionResult
+    {
+        public double bytesUsed = 0;
+
+        public JsonResult(ref object obj, ref int status, ref string message)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                using (StringWriter writer = new StringWriter(sb))
+                {
+                    using (JsonWriter jsonWriter = new JsonTextWriter(writer))
+                    {
+                        var serializer = new JsonSerializer();
+                        serializer.ApplyCustomSettings();
+                        serializer.Serialize(writer, obj);
+                    }
+                }
+
+                string json = sb.ToString();//JsonConvert.SerializeObject(obj);
+                this.Content = json;
+            }
+            catch (Exception ex)
+            {
+                Status = ResponseStatus.ERROR;
+                Message = ex.Message;
+
+                return;
+            }
+
+            Status = status;
+            Message = message;
+        }
+    }
+}
