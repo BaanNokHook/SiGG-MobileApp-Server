3df957e6e30f7113b9e8c84fb9223d1efcf455f6
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-18 09:01:00 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerObjects-ServerSocketResponse

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerObjects/ServerSocketResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerObjects
+{
+    internal class ServerSocketResponse
+    {
+        public int status { get; set; }
+        public string message { get; set; }
+        public object entity { get; set; }
+
+        public ServerSocketResponse(int status,
+            string message, object entity)
+        {
+            this.status = status;
+            this.message = message;
+            this.entity = entity;
+        }
+    }
+}
