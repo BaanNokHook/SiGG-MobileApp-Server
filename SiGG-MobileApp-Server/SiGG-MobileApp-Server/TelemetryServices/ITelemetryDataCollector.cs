2b0c185da4a84525912da0f2cbb07b8be89c19d1
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-18 18:09:51 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-TelemetryServices-ITelemetryDataCollector

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/TelemetryServices/ITelemetryDataCollector.cs
@@ -0,0 +1,21 @@
+﻿using SiGG_MobileApp_Server.TelemetryServices.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.TelemetryServices
+{
+    internal interface ITelemetryDataCollector
+    {
+        void Collect(object eventObj);   
+        
+            IEnumerable<ActionError> GetActionErros();
+            IEnumerable<ActionExecutionTime> GetActionExecutions();
+            IEnumerable<DependencyInjectorExecutionTime> GetDependencyInjectors();
+            IEnumerable<HardwareUsage> GetHardwareUsages();
+            IEnumerable<InterceptorExecutionTime> GetInterceptorExecutions();
+        
+    }
+}
