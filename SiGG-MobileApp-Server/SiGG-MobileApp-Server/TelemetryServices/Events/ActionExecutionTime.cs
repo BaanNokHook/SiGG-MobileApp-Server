df8d8b7ce4348cc51b27aaf65de2962d09e27ed9
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-18 14:33:47 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-TelemetryServices-Events-ActionExecutionTime

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/TelemetryServices/Events/ActionExecutionTime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.TelemetryServices.Events
+{
+    public struct ActionExecutionTime
+    {
+        public string ControllerName { get; private set; }    
+        
+        public string ActionName { get; private set; }   
+
+        private string ElapsedTime { get; private set; }   
+
+        public DateTime CollectTime { get; private set; }    
+
+        public ActionExecutionTime(string controllerName,  
+            string actionName, 
+            long elapsedTime)
+        {
+            ControllerName = controllerName;
+            ActionName = actionName;
+            ElapsedTime = $"{elapsedTime} ms";
+            CollectTime = DateTime.Now;   
+        }
+    }
+}
