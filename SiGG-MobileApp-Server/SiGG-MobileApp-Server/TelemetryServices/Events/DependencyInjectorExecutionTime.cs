1835a52eba8db0a52d9d1093afb7e8b0baf0d071
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-18 14:41:37 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-TelemetryServices-Events-DependencyInjectorExecutionTime

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/TelemetryServices/Events/DependencyInjectorExecutionTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.TelemetryServices.Events
+{
+    public struct DependencyInjectorExecutionTime   
+    {
+        public string ControllerName { get; private set; }   
+
+        public string ElapsedMs { get; }   
+
+        public DateTime CalledTime { get; private set; }
+
+        public DependencyInjectorExecutionTime(string controllerName,
+            long elapsedMs)
+        {
+            ControllerName = controllerName;
+            ElapsedMs = $"{elapsedMs} ms";
+            CalledTime = DateTime.Now;
+        }
+    }
+}
