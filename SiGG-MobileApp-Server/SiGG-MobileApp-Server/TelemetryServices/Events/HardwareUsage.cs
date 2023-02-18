c74ab6aee04e621a93c7e2846eaa645ccd7639b1
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-18 14:50:31 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-TelemetryServices-Events-HardwareUsage

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/TelemetryServices/Events/HardwareUsage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.TelemetryServices.Events
+{
+    public struct HardwareUsage
+    {
+        public double CPUUsage { get; private set; }   
+
+        public double MemoryUsageMegabytes { get; private set; }   
+
+        public double CurrentThreadCount { get; private set; }   
+
+        public DateTime CollectedTime { get; private set; }   
+        
+        public HardwareUsage(double cpuUsage, double memoryUsage,   
+            double currentThradCount)
+        {
+            CPUUsage = cpuUsage;
+            MemoryUsageMegabytes = memoryUsage;
+            CurrentThreadCount = currentThradCount;
+            CollectedTime = DateTime.Now;    
+        }
+    }
+}
