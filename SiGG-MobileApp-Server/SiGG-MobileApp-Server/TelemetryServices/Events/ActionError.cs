d0d9afa64bfc04f4e5eb789e6f2a01413765b3b0
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-18 14:17:32 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-TelemetryServices-Events-ActionError

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/TelemetryServices/Events/ActionError.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.TelemetryServices.Events
+{
+    public struct ActionError
+    { 
+        public string ControllerName { get; private set; }     
+        public string ActionName { get; private set;  }   
+
+        public DateTime CalledTime { get; private set; }    
+
+        public string Error { get; private set; }   
+
+        public ActionError(string controllerName, string actionName, string error)
+        {
+            ControllerName = controllerName;
+            ActionName = actionName;
+            Error = error;
+            CalledTime = DateTime.Now;   
+        }
+    }
+}
