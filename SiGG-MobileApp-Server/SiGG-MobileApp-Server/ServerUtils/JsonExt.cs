43fe8da083f0b08627d649bb075d3d9ff7fe4256
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-18 12:15:20 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerUtils-JsonExt

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerUtils/JsonExt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerUtils
+{
+    public static class JsonExt
+    {
+        public static void ApplyCustomSettings(this JsonSerializer serializer)
+        {
+            if (AppServerConfigurator.SerializerSettings == null)
+                return;
+
+            var settings = AppServerConfigurator.SerializerSettings;
+
+            foreach (PropertyInfo prop in serializer.GetType().GetProperties())
+            {
+                try
+                {
+                    var value = settings.GetType().GetProperty(prop.Name).GetValue(settings);
+                    if (value == null)
+                        continue;
+                    prop.SetValue(serializer, value);
+                }
+                catch { }
+            }
+        }
+    }
+}
