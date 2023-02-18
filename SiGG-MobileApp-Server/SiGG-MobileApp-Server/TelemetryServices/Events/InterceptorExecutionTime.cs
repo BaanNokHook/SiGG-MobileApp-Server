3da51b8f3703e2998f1ff2f6d03681f62833e1b9
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-18 15:31:35 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-TelemetryServices-Events-InterceptorExecutionTime

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/TelemetryServices/Events/InterceptorExecutionTime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.TelemetryServices.Events
+{
+    public struct InterceptorExecutionTime
+    {
+        public string ControllerName { get; private set; }
+
+        public string ActionName { get; private set; }
+
+        public DateTime CollectTime { get; private set; }
+
+        public string ElapsedMs { get; private set; }
+
+        public InterceptorExecutionTime(string controllerName, string actionName,
+            long elapsedMs)
+        {
+            ControllerName = controllerName;
+            ActionName = actionName;
+            ElapsedMs = $"{elapsedMs} ms";
+            CollectTime = DateTime.Now;
+        }
+    }
+}
