42287bdcc33b20983000a243f03bff39c776012b
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-18 13:17:55 +0700

    ServiceHelperSiGG-MobileApp-Server-Service-ServerUtils-ParameterRebinder

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ServerUtils/ParameterRebinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ServerUtils
+{
+    public class ParameterRebinder : ExpressionVisitor
+    {
+        private readonly Dictionary<ParameterExpression, ParameterExpression> map;
+
+        public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
+        {
+            this.map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();   
+        }  
+
+        public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
+        {
+            return new ParameterRebinder(map).Visit(exp);   
+        }  
+
+        protected override Expression VisitParameter(ParameterExpression p)
+        {
+            ParameterExpression replacement;
+            if (map.TryGetValueGet(p, out replacement))
+                p = replacement;
+
+            return base.VisitParameter(p);   
+        }
+    }
+}
