fd5066782374778fd85b995119b0e49a6ebd9768
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-19 11:01:52 +0700

    SiGG-MobileApp-Server-SocketAppServerClient.Standard-ClientUtils-ServerResponse

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/SocketAppServerClient.Standard/ClientUtils/ServerResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.SocketAppServerClient.Standard.ClientUtils
+{
+    public class ServerResponse
+    {
+        public double ResponseLenght { get; set; }
+        public double PercentUsage { get; set; }
+        public string FileState { get; set; }
+        public int Type { get; set; }
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public object Content { get; set; }
+
+        public string ServerMessage { get; set; }
+
+        public ServerResponse(int status,
+            string message, object entity, double bytesUsed)
+        {
+            this.Status = status;
+            this.Message = message;
+            this.Content = entity;
+            ResponseLenght = bytesUsed;
+        }
+
+        public ServerResponse()
+        {
+
+        }
+    }
+}
