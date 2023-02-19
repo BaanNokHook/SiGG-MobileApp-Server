458dcae7d5ec3ec9daf0bd4073d0cfd1a4a7fb4f
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-19 10:20:46 +0700

    SiGG-MobileApp-Server-SocketAppServerClient.Standard-ClientUtils-ServerInfo

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/SocketAppServerClient.Standard/ClientUtils/ServerInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.SocketAppServerClient.Standard.ClientUtils
+{
+    public class ServerInfo
+    {
+        [JsonProperty]
+        public string MachineName { get; private set; }
+
+        [JsonProperty]
+        public string ServerVersion { get; private set; }
+
+        [JsonProperty]
+        public bool RequiresAuthentication { get; private set; }
+
+        [JsonProperty]
+        public bool IsLoadBanancingServer { get; private set; }
+
+        [JsonProperty]
+        public List<ControllerInfo> ServerControllers { get; private set; }
+
+        public ServerInfo()
+        {
+            ServerControllers = new List<ControllerInfo>();
+        }
+    }
+}
