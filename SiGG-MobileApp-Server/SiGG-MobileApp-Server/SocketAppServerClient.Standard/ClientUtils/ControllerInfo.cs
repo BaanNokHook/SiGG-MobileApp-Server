20d63369e590f615b9eadebce3897eb717b5141b
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-19 09:28:37 +0700

    SiGG-MobileApp-Server-SocketAppServerClient.Standard-ClientUtils-ControllerInfo

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/SocketAppServerClient.Standard/ClientUtils/ControllerInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.SocketAppServerClient.Standard.ClientUtils
+{
+    public class ControllerInfo
+    {
+        [JsonProperty]
+        public string ControllerName { get; private set; }
+        [JsonProperty]
+        public string ControllerClass { get; private set; }
+        [JsonProperty]
+        public List<string> ControllerActions { get; private set; }
+
+        public ControllerInfo()
+        {
+            ControllerActions = new List<string>();
+        }
+    }
+}
