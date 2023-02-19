3a980fa5a11888e79d5f59fd7dc8e5d09f6ce74f
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-19 13:02:31 +0700

    SiGG-MobileApp-Server-SocketAppServerClient.Standard-ClientUtils-StatusResult

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/SocketAppServerClient.Standard/ClientUtils/StatusResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.SocketAppServerClient.Standard.ClientUtils
+{
+    public enum StatusResult
+    {
+        OPERACAO_OK = 1,
+        FALHA_VALIDACAO = 2,
+        FALHA_INTERNA = 3,
+        NAO_ENCONTRADO = 4
+    }
+}
