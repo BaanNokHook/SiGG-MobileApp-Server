2b8faad35bf4a1511f14b3bf1694305fb08eaada
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-20 19:27:15 +0700

    SiGG-MobileApp-Server-MobileAppServerClient-OperationResult

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/MobileAppServerClient/OperationResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.MobileAppServerClient
+{
+
+    public class OperationResult
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public object Entity { get; set; }
+    }
+}
