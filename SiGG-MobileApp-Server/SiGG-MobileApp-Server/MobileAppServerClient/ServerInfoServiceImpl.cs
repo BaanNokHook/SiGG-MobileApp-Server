76c07aaa75b373e917ec565a8e3a08f8e65feea2
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-20 20:56:06 +0700

    SiGG-MobileApp-Server-MobileAppServerClient-ServerInfoServiceImpl

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/MobileAppServerClient/ServerInfoServiceImpl.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.MobileAppServerClient
+{
+    internal class ServerInfoServiceImpl : IServerInformationService
+    {
+        ISocketClientConnection connection;
+
+        public ServerInfoServiceImpl(ISocketClientConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public double ServerAverageCPUUsage(int lastMinutes = 3)
+        {
+            connection.SendRequest("ServerInfoController", "AverageCPUUsage", new
+            {
+                lastMinutes
+            });
+
+            return connection.GetResultObject<double>();
+        }
+
+        public double ServerAverageMemoryUsage(int lastMinutes = 3)
+        {
+            connection.SendRequest("ServerInfoController", "AverageMemoryUsage", new
+            {
+                lastMinutes
+            });
+
+            return connection.GetResultObject<double>();
+        }
+
+        public int RequestsErrorsCount(string controllerName, string actionName)
+        {
+            connection.SendRequest("ServerInfoController", "RequestsErrorsCount", new
+            {
+                controllerName,
+                actionName
+            });
+
+            return connection.GetResultObject<int>();
+        }
+
+        public int RequestsSuccessCount(string controllerName, string actionName)
+        {
+            connection.SendRequest("ServerInfoController", "RequestsSuccessCount", new
+            {
+                controllerName,
+                actionName
+            });
+
+            return connection.GetResultObject<int>();
+        }
+
+        public long GetCurrentThreadsCount()
+        {
+            connection.SendRequest("ServerInfoController", "GetCurrentThreadsCount");
+            return connection.GetResultObject<long>();
+        }
+
+        public string[] GetActionParameters(string controller, string action)
+        {
+            connection.SendRequest("ServerInfoController", "GetActionParameters", new
+            {
+                controller,
+                action
+            });
+
+            return connection.GetResultObject<string[]>();
+        }
+
+        public ServerInfo GetFullServerInfo()
+        {
+            connection.SendRequest("ServerInfoController", "FullServerInfo");
+            return connection.GetResultObject<ServerInfo>();
+        }
+    }
+}
