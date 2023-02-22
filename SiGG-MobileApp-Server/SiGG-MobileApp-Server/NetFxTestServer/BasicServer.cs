bab56e6a95b43ea901003bd4cb705f80fc36373d
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-22 10:40:23 +0700

    SiGG-MobileApp-Server-MobileAppServerTest-NetFxTestServer-BasicServer

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/NetFxTestServer/BasicServer.cs
@@ -0,0 +1,22 @@
+﻿using SocketAppServer.CoreServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.NetFxTestServer
+{
+    public class BasicServer : IBasicServerController
+    {
+        public object RunAction(string receivedData, SocketRequest request)
+        {
+            request.SendData("m                                                                               ");
+
+
+            return @"<STX>
+<L>
+<ETX>";
+        }
+    }
+}
