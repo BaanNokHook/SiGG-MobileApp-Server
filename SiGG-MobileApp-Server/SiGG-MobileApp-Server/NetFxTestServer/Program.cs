7c8cecc747b2ebc5699772388aa15989a1c1c1ea
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-22 13:08:42 +0700

    SiGG-MobileApp-Server-MobileAppServerTest-NetFxTestServer-Program

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/NetFxTestServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using SiGG_MobileApp_Server.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.NetFxTestServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Client.Configure("localhost", 4050, Encoding.UTF8, 1000000);
+            Handle();
+            */
+            Console.ForegroundColor = ConsoleColor.White;
+            SocketServerHost.CreateHostBuilder()
+                   .UseStartup<Startup>()
+                   .Run();
+        }
+    }
+}
