1f1aa63f60d83ffd7a34e8132d2b0b7ead1b5734
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-22 08:23:07 +0700

    SiGG-MobileApp-Server-MobileAppServerTest-Program

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/MobileAppServerTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SiGG_MobileApp_Server.MobileAppServerTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        static void Xpto(object obj)
+        {
+            var props = obj.GetType().GetProperties();
+        }
+    }
+}
