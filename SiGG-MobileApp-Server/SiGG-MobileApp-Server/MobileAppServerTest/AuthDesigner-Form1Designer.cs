883ddc5163a405c5c5af663751d78b396ece29c8
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-22 08:17:05 +0700

    SiGG-MobileApp-Server-MobileAppServerTest-AuthDesigner-Form1Designer

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/MobileAppServerTest/AuthDesigner-Form1Designer.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.MobileAppServerTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btConnect = new System.Windows.Forms.Button();
+            this.txPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txAddress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.lbDescription = new System.Windows.Forms.Label();
+            this.txResult = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.ckSaveToFile = new System.Windows.Forms.CheckBox();
+            this.lbBytes = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.btConnect);
+            this.panel1.Controls.Add(this.txPort);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txAddress);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(711, 67);
+            this.panel1.TabIndex = 0;
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(440, 33);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(44, 20);
+            this.btConnect.TabIndex = 5;
+            this.btConnect.Text = "Go!";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // txPort
+            // 
+            this.txPort.Location = new System.Drawing.Point(366, 33);
+            this.txPort.Name = "txPort";
+            this.txPort.Size = new System.Drawing.Size(68, 20);
+            this.txPort.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(334, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Port";
+            // 
+            // txAddress
+            // 
+            this.txAddress.Location = new System.Drawing.Point(84, 33);
+            this.txAddress.Name = "txAddress";
+            this.txAddress.Size = new System.Drawing.Size(244, 20);
+            this.txAddress.TabIndex = 2;
+            this.txAddress.Text = "localhost";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connect to you server";
+            // 
+            // treeView
+            // 
+            this.treeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView.Location = new System.Drawing.Point(0, 98);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(238, 353);
+            this.treeView.TabIndex = 1;
+            this.treeView.DoubleClick += new System.EventHandler(this.treeView_DoubleClick);
+            // 
+            // lbDescription
+            // 
+            this.lbDescription.AutoSize = true;
+            this.lbDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDescription.Location = new System.Drawing.Point(9, 73);
+            this.lbDescription.Name = "lbDescription";
+            this.lbDescription.Size = new System.Drawing.Size(148, 20);
+            this.lbDescription.TabIndex = 2;
+            this.lbDescription.Text = "DISCONNECTED";
+            // 
+            // txResult
+            // 
+            this.txResult.AcceptsReturn = true;
+            this.txResult.AcceptsTab = true;
+            this.txResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txResult.Location = new System.Drawing.Point(245, 98);
+            this.txResult.Multiline = true;
+            this.txResult.Name = "txResult";
+            this.txResult.Size = new System.Drawing.Size(454, 311);
+            this.txResult.TabIndex = 3;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(242, 428);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(99, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Create new request";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // ckSaveToFile
+            // 
+            this.ckSaveToFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ckSaveToFile.AutoSize = true;
+            this.ckSaveToFile.Location = new System.Drawing.Point(496, 429);
+            this.ckSaveToFile.Name = "ckSaveToFile";
+            this.ckSaveToFile.Size = new System.Drawing.Size(203, 17);
+            this.ckSaveToFile.TabIndex = 5;
+            this.ckSaveToFile.Text = "Save to File, not show in this Window";
+            this.ckSaveToFile.UseVisualStyleBackColor = true;
+            // 
+            // lbBytes
+            // 
+            this.lbBytes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbBytes.AutoSize = true;
+            this.lbBytes.Location = new System.Drawing.Point(244, 412);
+            this.lbBytes.Name = "lbBytes";
+            this.lbBytes.Size = new System.Drawing.Size(177, 13);
+            this.lbBytes.TabIndex = 6;
+            this.lbBytes.Text = "Bytes used: 0 (0,0% of server buffer)";
+            this.lbBytes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbBytes.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 450);
+            this.Controls.Add(this.lbBytes);
+            this.Controls.Add(this.ckSaveToFile);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.txResult);
+            this.Controls.Add(this.lbDescription);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.TextBox txPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txAddress;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Label lbDescription;
+        private System.Windows.Forms.TextBox txResult;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.CheckBox ckSaveToFile;
+        private System.Windows.Forms.Label lbBytes;
+    }
+}
+
