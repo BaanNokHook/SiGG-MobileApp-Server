0c6eda54158b56134f323d1ff1fae92afbac42a1
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-22 08:28:33 +0700

    SiGG-MobileApp-Server-MobileAppServerTest-RequestCache

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/MobileAppServerTest/RequestCache.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.MobileAppServerTest
+{
+    public class RequestCache
+    {
+        public string Controller { get; set; }
+
+        public string Action { get; set; }
+
+        public List<ServerRequestParameter> Parameters { get; set; }
+
+        public RequestCache()
+        {
+            Parameters = new List<ServerRequestParameter>();
+        }
+    }
+}
