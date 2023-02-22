aac202fd74c501b6bd8a59d3f9c9331ff4a211e4
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-22 08:32:26 +0700

    SiGG-MobileApp-Server-MobileAppServerTest-ServerRequestParameter

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/MobileAppServerTest/ServerRequestParameter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.MobileAppServerTest
+{
+    public class ServerRequestParameter
+    {
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
