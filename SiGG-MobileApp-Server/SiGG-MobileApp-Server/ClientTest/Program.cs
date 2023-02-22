73b2307225e655389fa2493b68ff68b9c052933f
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-22 15:01:33 +0700

    SiGG-MobileApp-Server-MobileAppServerTest-ClientTest-Program

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/ClientTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.ClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SocketConnectionFactory.SetDefaultSettings(new SocketClientSettings(
+                        "localhost", 7001,
+                        Encoding.UTF8, 3
+                    ));
+
+                using (ISocketClientConnection conn = SocketConnectionFactory.GetConnection())
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+}
