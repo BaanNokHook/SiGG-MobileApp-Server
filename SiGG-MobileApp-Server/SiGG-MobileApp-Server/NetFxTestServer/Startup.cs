41ee0d3c4936c8b7093b36525ba76cdd5e92c8a4
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-22 13:22:02 +0700

    SiGG-MobileApp-Server-MobileAppServerTest-NetFxTestServer-Startup

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/NetFxTestServer/Startup.cs
@@ -0,0 +1,25 @@
+﻿using SiGG_MobileApp_Server.CoreServices.CoreServer;
+using SiGG_MobileApp_Server.ManagedServices;
+using SocketAppServer.CoreServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.NetFxTestServer
+{
+    public class Startup : AppServerConfigurator
+    {
+        public override void ConfigureServices(IServiceManager serviceManager)
+        {
+            ICoreServerService coreServer = serviceManager.GetService<ICoreServerService>("realserver");
+            coreServer.EnableBasicServerProcessorMode(typeof(BasicServer));
+        }
+
+        public override ServerConfiguration GetServerConfiguration()
+        {
+            return new ServerConfiguration(Encoding.ASCII, 1001);
+        }
+    }
+}
