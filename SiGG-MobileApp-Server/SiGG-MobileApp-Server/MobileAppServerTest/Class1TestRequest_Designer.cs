6f49a8cb8f99007523171c7377a185cd8f4fc031
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-22 10:28:33 +0700

    SiGG-MobileApp-Server-MobileAppServerTest-TestRequest_Designer

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/MobileAppServerTest/Class1TestRequest_Designer.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.MobileAppServerTest
+{
+    partial class TestRequest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txController = new System.Windows.Forms.TextBox();
+            this.txAction = new System.Windows.Forms.TextBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ckFileResult = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txMemoryStorageId = new System.Windows.Forms.TextBox();
+            this.txChunkResponseSize = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.keyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.serverRequestParameterBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txChunkResponseSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.serverRequestParameterBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Send new request to your server\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Controller Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Action Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Parameters:";
+            // 
+            // txController
+            // 
+            this.txController.Location = new System.Drawing.Point(121, 47);
+            this.txController.Name = "txController";
+            this.txController.Size = new System.Drawing.Size(192, 20);
+            this.txController.TabIndex = 4;
+            // 
+            // txAction
+            // 
+            this.txAction.Location = new System.Drawing.Point(121, 73);
+            this.txAction.Name = "txAction";
+            this.txAction.Size = new System.Drawing.Size(192, 20);
+            this.txAction.TabIndex = 5;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGrid.AutoGenerateColumns = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.keyDataGridViewTextBoxColumn,
+            this.valueDataGridViewTextBoxColumn});
+            this.dataGrid.DataSource = this.serverRequestParameterBindingSource;
+            this.dataGrid.Location = new System.Drawing.Point(12, 200);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGrid.Size = new System.Drawing.Size(326, 259);
+            this.dataGrid.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 465);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(326, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ckFileResult
+            // 
+            this.ckFileResult.AutoSize = true;
+            this.ckFileResult.Location = new System.Drawing.Point(121, 100);
+            this.ckFileResult.Name = "ckFileResult";
+            this.ckFileResult.Size = new System.Drawing.Size(182, 17);
+            this.ckFileResult.TabIndex = 8;
+            this.ckFileResult.Text = "This action response a FileResult";
+            this.ckFileResult.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Save response into:";
+            // 
+            // txMemoryStorageId
+            // 
+            this.txMemoryStorageId.Location = new System.Drawing.Point(124, 126);
+            this.txMemoryStorageId.Name = "txMemoryStorageId";
+            this.txMemoryStorageId.Size = new System.Drawing.Size(192, 20);
+            this.txMemoryStorageId.TabIndex = 10;
+            // 
+            // txChunkResponseSize
+            // 
+            this.txChunkResponseSize.Location = new System.Drawing.Point(124, 152);
+            this.txChunkResponseSize.Maximum = new decimal(new int[] {
+            999999,
+            0,
+            0,
+            0});
+            this.txChunkResponseSize.Name = "txChunkResponseSize";
+            this.txChunkResponseSize.Size = new System.Drawing.Size(79, 20);
+            this.txChunkResponseSize.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Chunk response in:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(209, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "characters per part";
+            // 
+            // keyDataGridViewTextBoxColumn
+            // 
+            this.keyDataGridViewTextBoxColumn.DataPropertyName = "Key";
+            this.keyDataGridViewTextBoxColumn.HeaderText = "Key";
+            this.keyDataGridViewTextBoxColumn.Name = "keyDataGridViewTextBoxColumn";
+            // 
+            // valueDataGridViewTextBoxColumn
+            // 
+            this.valueDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.valueDataGridViewTextBoxColumn.DataPropertyName = "Value";
+            this.valueDataGridViewTextBoxColumn.HeaderText = "Value";
+            this.valueDataGridViewTextBoxColumn.Name = "valueDataGridViewTextBoxColumn";
+            // 
+            // serverRequestParameterBindingSource
+            // 
+            this.serverRequestParameterBindingSource.DataSource = typeof(MobileAppServerTest.ServerRequestParameter);
+            // 
+            // TestRequest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(350, 500);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txChunkResponseSize);
+            this.Controls.Add(this.txMemoryStorageId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ckFileResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.txAction);
+            this.Controls.Add(this.txController);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TestRequest";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TestRequest";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txChunkResponseSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.serverRequestParameterBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txController;
+        private System.Windows.Forms.TextBox txAction;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.BindingSource serverRequestParameterBindingSource;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn keyDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.CheckBox ckFileResult;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txMemoryStorageId;
+        private System.Windows.Forms.NumericUpDown txChunkResponseSize;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
