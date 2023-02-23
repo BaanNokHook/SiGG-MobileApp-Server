9da8ee83e32bb1326c1845d16d4b5b2f7eb90bd3
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-23 18:09:58 +0700

    SiGG-MobileApp-Server-MobileAppServerTest-AnAnyExtensionFromFile-FunnyCommand

---
+++ SiGG-MobileApp-Server/SiGG-MobileApp-Server/AnAnyExtensionFromFile/FunnyCommand.cs
@@ -0,0 +1,21 @@
+﻿using SiGG_MobileApp_Server.CoreServices.CLIHost;
+using SiGG_MobileApp_Server.ManagedServices;
+using SocketAppServer.CoreServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGG_MobileApp_Server.AnAnyExtensionFromFile
+{
+    public class FunnyCommand : ICLIClient
+    {
+        public void Activate()
+        {
+            IServiceManager manager = ServiceManager.GetInstance();
+            ILoggingService logging = manager.GetService<ILoggingService>();
+            logging.WriteLog("Hi! Im a simple CLI function running inside this beautiful extension loaded dinamically! :)");
+        }
+    }
+}
